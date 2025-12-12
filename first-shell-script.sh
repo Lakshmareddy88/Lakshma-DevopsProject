dc4269fe3a2d9794aee80fe0431f4766a16d12d7
Ubuntu <ubuntu@ip-172-31-72-248.ec2.internal>
2025-12-12 03:37:02 +0000

    Add first-shell-script.sh Script to Git

---
+++ first-shell-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#Author: Lakshma
+#Date: 12th Dec 2025
+#
+#This script is used for push to GitHub
+#Version: V1
+#####################
+echo "print the disck space"
+df -h
+
+echo "print the memory"
+free -g
+
+echo "print the cpu
+nproc
+
